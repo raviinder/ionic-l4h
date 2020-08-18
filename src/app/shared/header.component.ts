f8f73a12f3c0dca96810c5bfaa6fd135ecf67f4f
Pavan Patel <patelpavan97@gmail.com>
2020-08-18 15:21:09 -0400

    Header Modification

--- src/app/shared/header.component.ts
+++ src/app/shared/header.component.ts
@@ -6,19 +6,42 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
+
+
 export class HeaderComponent implements OnInit {
 
   currentUrl: string;
+   
   constructor(private router: Router) {}
+
+  isMobile = false;
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 992;
+    console.log(w);
+    if (w < breakpoint) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   ngOnInit() {
     this.currentUrl = this.router.url;
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => {
+      this.isMobile = this.getIsMobile();
+    };
   }
+
+  
 
   buttons = [
     {title: 'Home', url: '/menu/home'},
     {title: 'Events', url: '/menu/events'},
     {title: 'Donate', url: '/menu/donate'},
   ]
-
 }
+
+
+
